5672603b9d754b72e758759f3f4dc0a1e4a96e84
Eduardo Vargas Torres <evargast@adobe.com>
2020-08-14 15:42:33 -0500

    V1.0.6 (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Theme } from '@react-types/provider';
 
-import lightest from '@spectrum-css/vars/dist/spectrum-lightest.css';
 import global from '@spectrum-css/vars/dist/spectrum-global.css';
+import lightest from '@spectrum-css/vars/dist/spectrum-lightest.css';
 import large from '@spectrum-css/vars/dist/spectrum-large.css';
 import medium from '@spectrum-css/vars/dist/spectrum-medium.css';
 
-
+import '@spectrum-css/vars/css/globals/index.css';
+import '@spectrum-css/vars/css/themes/spectrum-lightest.css';
+import '@spectrum-css/vars/css/scales/spectrum-large.css';
+import '@spectrum-css/vars/css/scales/spectrum-medium.css';
 
 const WhiteTheme: Theme = {
   global,
